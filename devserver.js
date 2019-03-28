cf587abd380455a5502d986fa2c1149869b987a8
Quang Tran <quang.tran@numberfour.eu>
2019-03-28 12:03:57 +0100

    Initial commit

---
+++ devserver.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+
+const config = {
+  devtool: 'source-map',
+
+  entry:   [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src-gen/client/main.js' // Vote App
+  ],
+  output:  {
+    path:       path.join(__dirname, 'public/dist'),
+    filename:   'vote-app.js',
+    publicPath: '/dist'
+  },
+  resolve: {
+	alias: {
+      vote: __dirname
+     }
+  },
+  module:  {
+    loaders: [
+      {
+        test:    /\.jsx?/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src-gen/client')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+
+};
+
+new WebpackDevServer(webpack(config), {
+  publicPath:         config.output.publicPath,
+  contentBase:        path.join(__dirname, 'public'),
+  hot:                true,
+  historyApiFallback: true
+}).listen(8080, 'localhost', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Listening at localhost:8080');
+});
